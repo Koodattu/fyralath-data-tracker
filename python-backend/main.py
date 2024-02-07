6827e9435d81d52935b1b057e46bea3c9bb71a2d
Juha Ala-Rantala <alarantala.juha@gmail.com>
2024-02-07 23:40:13 +0200

    Add HTML and JavaScript code for fetching and displaying auction house prices

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 from flask_caching import Cache
+from flask_cors import CORS
 import json
 from auction_data_fetcher import AuctionDataFetcher
 import schedule
 import threading
 import time
+import socket
 
 app = Flask(__name__)
+CORS(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 auction_fetcher = AuctionDataFetcher()
 
@@ -33,6 +36,19 @@ def get_data():
         data = json.load(file)
     return data
 
+def get_local_ip():
+    """Function to get the local IP address of the machine."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '0.0.0.0'
+    finally:
+        s.close()
+    return IP
+
 if __name__ == '__main__':
     print("Starting Flask app")
     # Start the scheduler thread
@@ -42,7 +58,9 @@ if __name__ == '__main__':
     scheduler_thread = threading.Thread(target=run_scheduler)
     scheduler_thread.start()
 
-    # Start the Flask app
-    app.run()
+    # Start the Flask app using the local IP address
+    local_ip = get_local_ip()
+    print(f"Flask app starting on http://{local_ip}:5000")
+    app.run(host=local_ip)
 
     print("Flask app started")
