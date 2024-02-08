54dcdf7ada0bb14a9a6410d748d59013dd455a9a
Juha Ala-Rantala <alarantala.juha@gmail.com>
2024-02-08 11:17:15 +0200

    fixed file saving

--- python-backend/auction_data_fetcher.py
+++ python-backend/auction_data_fetcher.py
@@ -43,6 +43,9 @@ class AuctionDataFetcher:
 
     def save_data_as_json(self, filename, data):
         """Saves the fetched data as a JSON file."""
+        directory = os.path.dirname(filename)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         try:
             with open(filename, 'w') as file:
                 json.dump(data, file)
@@ -51,7 +54,7 @@ class AuctionDataFetcher:
 
     def entry_exists_for_current_hour(self, region):
         """Checks if an entry for the current year, month, day, and hour exists in the file."""
-        filename = f'/data/total_cost_{region}.json'
+        filename = f'./data/total_cost_{region}.json'
         now = datetime.datetime.now().isoformat()
         current_date_hour = now[:13]
 
@@ -70,11 +73,14 @@ class AuctionDataFetcher:
 
     def update_total_cost_file(self, region, total_cost):
         """Updates the total cost file for the region with a new data entry."""
-        filename = f'/data/total_cost_{region}.json'
+        filename = f'./data/total_cost_{region}.json'
         now = datetime.datetime.now().isoformat()
         entry = {'timestamp': now, 'total_cost': total_cost}
         
         try:
+            if not os.path.exists(os.path.dirname(filename)):
+                os.makedirs(os.path.dirname(filename))
+
             if os.path.exists(filename):
                 with open(filename, 'r+') as file:
                     data = json.load(file)
@@ -85,7 +91,7 @@ class AuctionDataFetcher:
                 with open(filename, 'w') as file:
                     json.dump([entry], file)
 
-            backup_folder = '/data/backup'
+            backup_folder = './data/backup'
             os.makedirs(backup_folder, exist_ok=True)
             backup_path = os.path.join(backup_folder, f'total_cost_{region}.json')
             shutil.copy(filename, backup_path)
@@ -94,13 +100,29 @@ class AuctionDataFetcher:
 
     def save_latest_data(self, latest_data):
         """Saves the latest data for all regions into a single JSON file."""
-        filename = '/data/latest_total_costs.json'
+        filename = './data/latest_total_costs.json'
         try:
+            if not os.path.exists(os.path.dirname(filename)):
+                os.makedirs(os.path.dirname(filename))
+            
             with open(filename, 'w') as file:
                 json.dump(latest_data, file)
             print(f"Latest data saved to {filename}.")
         except Exception as e:
             print(f"Error saving latest data: {e}")
+
+    def calculate_total_cost(self, auction_data, parts_data):
+        """Calculates the total cost based on the lowest prices and amounts needed."""
+        item_id_to_name = {item['item_id']: item['item_name'] for part_data in parts_data.values() for item in part_data}
+        lowest_prices = {}
+        for auction in auction_data['auctions']:
+            item_id = auction['item']['id']
+            unit_price = auction['unit_price']
+            if item_id in item_id_to_name and (item_id not in lowest_prices or unit_price < lowest_prices[item_id]):
+                lowest_prices[item_id] = unit_price
+        total_costs = {part: sum(lowest_prices.get(item['item_id'], 0) * item['amount_needed'] for item in items) for part, items in parts_data.items()}
+        overall_total_cost = sum(total_costs.values())
+        return overall_total_cost, total_costs
 
     def run(self):
         client_id = os.getenv("CLIENT_ID")
@@ -126,7 +148,7 @@ class AuctionDataFetcher:
                 print(f"Failed to fetch auction data for {region} region. Skipping.")
                 continue
 
-            auction_data_filename = f'/data/auction_data/auction_data_{region}.json'
+            auction_data_filename = f'./data/auction_data/auction_data_{region}.json'
             self.save_data_as_json(auction_data_filename, auction_data)
             print(f'Auction data for {region} region saved to {auction_data_filename}.')
 
@@ -166,7 +188,6 @@ class AuctionDataFetcher:
             print("No new data fetched. Exiting.")
             return False
         
-        print(latest_data)
         self.save_latest_data(latest_data)
         return True
 
